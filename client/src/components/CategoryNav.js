2b9f9f0361853ac001dbe9ff86ad01202414b23d
G3RGES <Georgios.Nashaat@gmail.com>
2023-10-26 14:36:58 +0300

    Update CategoryNav.js

--- client/src/components/CategoryNav.js
+++ client/src/components/CategoryNav.js
@@ -1,6 +1,14 @@
-import React from 'react';
+import React from "react";
+
+//useFetch hook
+import useFetch from "../hooks/useFetch";
+
+//link
+import { Link } from "react-router-dom";
 
 const CategoryNav = () => {
+  const { data } = useFetch("/categories");
+  console.log(data);
   return <div>CategoryNav</div>;
 };
 
