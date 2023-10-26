34520dfdac5ec9854e6677c3598671d4ae8837d3
G3RGES <Georgios.Nashaat@gmail.com>
2023-10-26 12:26:31 +0300

    Update ProductSlider.js

--- client/src/components/ProductSlider.js
+++ client/src/components/ProductSlider.js
@@ -16,14 +16,34 @@ import Product from "../components/Product";
 const ProductSlider = ({ data }) => {
   // console.log(data);//*TESTING
   return (
-    <Swiper>
+    <Swiper
+      modules={[Pagination, Navigation]}
+      loop={false}
+      navigation={true}
+      breakpoints={{
+        320: {
+          slidesPerView: 1,
+          spaceBetween: 30,
+        },
+        768: {
+          slidesPerView: 2,
+          spaceBetween: 30,
+        },
+        1024: {
+          slidesPerView: 2,
+          spaceBetween: 30,
+        },
+        1440: {
+          slidesPerView: 5,
+          spaceBetween: 30,
+        },
+      }}
+    >
       <>
         {data?.map((product) => {
           return (
             <SwiperSlide key={product.id}>
-              <div className="w-full h-auto">
-                <Product product={product} />
-              </div>
+              <Product product={product} />
             </SwiperSlide>
           );
         })}
