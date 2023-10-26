c7d663ffc3a879de941812acbc79eb4a1bd7a13c
G3RGES <Georgios.Nashaat@gmail.com>
2023-10-26 13:49:33 +0300

    Update LatestProducts.js

--- client/src/components/LatestProducts.js
+++ client/src/components/LatestProducts.js
@@ -1,7 +1,22 @@
-import React from 'react';
+import React from "react";
+
+//component
+import ProductSlider from "./ProductSlider";
+
+//useFetch hook
+import useFetch from "../hooks/useFetch";
 
 const LatestProducts = () => {
-  return <div>LatestProducts</div>;
+  //get new products
+  const { data } = useFetch("/products?populate=*&filters[isNew]=true");
+  return (
+    <div className="mb-16">
+      <div className="container mx-auto">
+        <h2 className="h2 mb-6 text-center xl:text-left">latest Products</h2>
+      </div>
+      <ProductSlider data={data} />
+    </div>
+  );
 };
 
 export default LatestProducts;
