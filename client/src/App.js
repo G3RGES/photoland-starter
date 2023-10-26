e1f267c825251a794478a8290593f0681def306a
G3RGES <Georgios.Nashaat@gmail.com>
2023-10-26 08:27:23 +0300

    Update App.js

--- client/src/App.js
+++ client/src/App.js
@@ -1,4 +1,12 @@
-import React from 'react';
+import React from "react";
+
+import { createBroserRouter, RouterProvider, Outlet } from "react-router-dom";
+
+//pages
+import Home from "./pages/Home";
+import Products from "./pages/Home";
+import ProductDetails from "./pages/Home";
+import Search from "./pages/Home";
 
 const App = () => {
   return <div>App</div>;
