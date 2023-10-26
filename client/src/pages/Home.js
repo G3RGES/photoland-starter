b95c39c62d45a05a1143b930d74f0a7060cb0b20
G3RGES <Georgios.Nashaat@gmail.com>
2023-10-26 13:49:22 +0300

    Update Home.js

--- client/src/pages/Home.js
+++ client/src/pages/Home.js
@@ -1,22 +1,17 @@
 import React from "react";
 
 //import component
-import ProductSlider from "../components/ProductSlider";
-
-//useFetch hook
-import useFetch from "../hooks/useFetch.js";
+import LatestProducts from "../components/LatestProducts";
+import Hero from "../components/Hero";
 
 const Home = () => {
-  //get new products
-  const { data } = useFetch("/products?populate=*&filters[isNew]=true");
   // console.log(data);//*TESTING
+
   return (
-    <div className="mb-16">
-      <div className="container mx-auto">
-        <h2 className="h2 mb-6 text-center xl:text-left">latest Products</h2>
-      </div>
-      <ProductSlider data={data} />
-    </div>
+    <section>
+      <Hero />
+      <LatestProducts />
+    </section>
   );
 };
 
