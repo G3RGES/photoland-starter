5c9af7346832877aa1cdefa52a8107e9d7f60226
G3RGES <Georgios.Nashaat@gmail.com>
2023-10-26 10:35:08 +0300

    ADD strapi files

--- client/src/App.js
+++ client/src/App.js
@@ -1,6 +1,6 @@
 import React from "react";
 
-import { createBroserRouter, RouterProvider, Outlet } from "react-router-dom";
+import { createBrowserRouter, RouterProvider, Outlet } from "react-router-dom";
 
 //pages
 import Home from "./pages/Home";
@@ -8,8 +8,40 @@ import Products from "./pages/Home";
 import ProductDetails from "./pages/Home";
 import Search from "./pages/Home";
 
+//components
+import Header from "../src/components/Header";
+import Footer from "../src/components/Footer";
+
+//layout
+const Layout = () => {
+  return (
+    <div>
+      <Header />
+      <Outlet />
+      <Footer />
+    </div>
+  );
+};
+
+const router = createBrowserRouter([
+  {
+    element: <Layout />,
+    children: [
+      // { index: true, element: <Home /> },
+      { path: "/", element: <Home /> },
+      { path: "/products/:id", element: <Products /> },
+      { path: "/product/:id", element: <ProductDetails /> },
+      { path: "/search", element: <Search /> },
+    ],
+  },
+]);
+
 const App = () => {
-  return <div>App</div>;
+  return (
+    <div>
+      <RouterProvider router={router} />
+    </div>
+  );
 };
 
 export default App;
