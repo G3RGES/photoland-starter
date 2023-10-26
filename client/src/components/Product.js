95c3284cad172c85fb453b3b0b16f3039474678f
G3RGES <Georgios.Nashaat@gmail.com>
2023-10-26 12:26:27 +0300

    Update Product.js

--- client/src/components/Product.js
+++ client/src/components/Product.js
@@ -1,7 +1,24 @@
-import React from 'react';
+import React from "react";
 
-const Product = () => {
-  return <div>Product</div>;
+//link
+import { Link } from "react-router-dom";
+
+const Product = ({ product }) => {
+  // console.log(product); //TESTING
+  return (
+    <Link>
+      <div className="group">
+        <div className="w-full h-[200px] flex items-center justify-center relative">
+          {/* image */}
+          <img
+            className="w-[160px] h-[160px] group-hover:scale-90 transition-all"
+            src={`http://localhost:1337${product.attributes.image.data.attributes.url}`}
+            alt="Product"
+          />
+        </div>
+      </div>
+    </Link>
+  );
 };
 
 export default Product;
