16a77e2eea07b5464a51ad33c8801ac04f4da1ef
G3RGES <Georgios.Nashaat@gmail.com>
2023-10-27 12:38:27 +0200

    Update Header.js

--- client/src/components/Header.js
+++ client/src/components/Header.js
@@ -23,7 +23,7 @@ const Header = () => {
   return (
     <header className="bg-primary py-6 fixed w-full top-0 z-40 lg:relative xl:mb-[30px]">
       <div className="container mx-auto">
-        <div className="flex flex-row gap-4 lg:items-center justify-between mb-4 lg:mb-0">
+        <div className="flex flex-row gap-4 lg:items-center justify-between mb-4 xl:mb-0">
           {/* menu */}
           <div
             onClick={() => setCatNavMobile(true)}
@@ -79,7 +79,7 @@ const Header = () => {
           </div>
         </div>
         {/* search form -- show on mobile only */}
-        <div className="lg:hidden">
+        <div className="xl:hidden">
           <SearchForm />
         </div>
       </div>
