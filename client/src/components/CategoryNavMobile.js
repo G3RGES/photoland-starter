3d88b9dbaec1c1074c9f9e2a7c393ccfeed10e6a
G3RGES <Georgios.Nashaat@gmail.com>
2023-10-27 11:34:11 +0200

    Update CategoryNavMobile.js

--- client/src/components/CategoryNavMobile.js
+++ client/src/components/CategoryNavMobile.js
@@ -1,7 +1,23 @@
-import React from 'react';
+import React from "react";
 
-const CategoryNavMobile = () => {
-  return <div>CategoryNavMobile</div>;
+// icon
+import { FiX } from "react-icons/fi";
+
+const CategoryNavMobile = ({ setCatNavMobile }) => {
+  return (
+    <div className="w-full h-full bg-primary p-8">
+      {/* close icon */}
+      <div
+        className="flex justify-end mb-8"
+        // onClick={() => setCatNavMobile(false)} //*tutorial version but mine better😎
+      >
+        <FiX
+          onClick={() => setCatNavMobile(false)}
+          className="text-3xl cursor-pointer"
+        />
+      </div>
+    </div>
+  );
 };
 
 export default CategoryNavMobile;
