6b13ceee7821d3bf6120622f01dde96024c16dbb
G3RGES <Georgios.Nashaat@gmail.com>
2023-10-28 08:38:18 +0200

    Update RelatedProducts.js

--- client/src/components/RelatedProducts.js
+++ client/src/components/RelatedProducts.js
@@ -1,7 +1,30 @@
-import React from 'react';
+import React from "react";
 
-const RelatedProducts = () => {
-  return <div>RelatedProducts</div>;
+// useFetch hook
+import useFetch from "../hooks/useFetch";
+
+// components
+import ProductSlider from "./ProductSlider";
+
+const RelatedProducts = ({ categoryTitle }) => {
+  // console.log(categoryTitle);//TESTING
+
+  // get products based on category title
+  const { data } = useFetch(
+    `/products?populate=*&filters[categories][title]=${categoryTitle}`
+  );
+  // console.log(data);//TESTING
+
+  return (
+    <div className=" mb-16 ">
+      <div className="container mx-auto">
+        <h2 className="h2 text-accent mb-6 text-center xl:text-left font-semibold">
+          Related Products
+        </h2>
+        <ProductSlider data={data} />
+      </div>
+    </div>
+  );
 };
 
 export default RelatedProducts;
