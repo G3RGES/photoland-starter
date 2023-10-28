aeb7fd2278639f11e90787218e2998be6485cb84
G3RGES <Georgios.Nashaat@gmail.com>
2023-10-28 14:09:49 +0200

    Update Cart.js

--- client/src/components/Cart.js
+++ client/src/components/Cart.js
@@ -1,7 +1,7 @@
 import React, { useContext } from "react";
 
 // icons
-import { IoClose } from "react-icons/io5";
+import { IoArrowForward, IoCartOutline, IoClose } from "react-icons/io5";
 // context
 import { CartContext } from "../context/CartContext";
 
@@ -9,11 +9,11 @@ import { CartContext } from "../context/CartContext";
 import CartItem from "./CartItem";
 
 const Cart = () => {
-  const { setIsOpen, cart } = useContext(CartContext);
+  const { setIsOpen, cart, total, clearCart } = useContext(CartContext);
 
   return (
     <div className="w-full h-full px-4 text-white ">
-      <div>
+      <div className="overflow-y-auto overflow-x-hidden h-[75vh]">
         {/* close icon */}
         <div
           className="text-4xl w-20 h-[98px] flex justify-start items-center"
@@ -29,6 +29,55 @@ const Cart = () => {
             return <CartItem key={item.id} item={item} />;
           })}
         </div>
+      </div>
+      {/* subtotal & total */}
+      {cart.length >= 1 && (
+        <div className="px-6 py-2 flex flex-col">
+          {/* subtotal */}
+          <div className="flex flex-row justify-between text-lg">
+            <div>Subtotal:</div>
+            <div className="text-accent"> $ {parseFloat(total)}</div>
+          </div>
+          {/* total */}
+          <div className="flex flex-row justify-between text-2xl">
+            <div>Total:</div>
+            <div className="text-accent"> $ {total}</div>
+          </div>
+        </div>
+      )}
+      {/* button */}
+      <div className="px-6 py-6">
+        {cart.length >= 1 ? (
+          <div className="uppercase flex justify-between gap-x-4">
+            <button
+              className="btn btn-accent hover:bg-accent-hover
+               hover:text-primary"
+              onClick={() => clearCart()}
+            >
+              Clear Cart
+            </button>
+            <button
+              className="btn btn-accent hover:bg-accent-hover
+             hover:text-primary flex-1
+              px-2 gap-x-2"
+            >
+              Checkout
+              <IoArrowForward className="text-lg" />
+            </button>
+          </div>
+        ) : (
+          <div
+            className="h-full absolute top-0 right-0 left-0
+          flex justify-center items-center z-10 flex-col"
+          >
+            <div className="uppercase text-2xl text-accent">
+              Your cart looks empty, Fill it up
+            </div>
+            <div className="uppercase text-6xl text-accent">
+              <IoCartOutline />
+            </div>
+          </div>
+        )}
       </div>
     </div>
   );
