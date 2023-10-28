949f16cd50cfea4cb4a80e0bbb35eda420d38c5e
G3RGES <Georgios.Nashaat@gmail.com>
2023-10-28 09:30:46 +0200

    Update Cart.js

--- client/src/components/Cart.js
+++ client/src/components/Cart.js
@@ -1,7 +1,29 @@
-import React from 'react';
+import React, { useContext } from "react";
+
+// icons
+import { IoClose } from "react-icons/io5";
+// context
+import { CartContext } from "../context/CartContext";
 
 const Cart = () => {
-  return <div>Cart</div>;
+  const { setIsOpen } = useContext(CartContext);
+
+  return (
+    <div className="w-full h-full px-4 text-white ">
+      <div>
+        {/* close icon */}
+        <div
+          className="text-4xl w-20 h-[98px] flex justify-start items-center"
+          // onClick={() => setIsOpen(false)}//* tutorial's version but mine is better😎
+        >
+          <IoClose
+            className="cursor-pointer"
+            onClick={() => setIsOpen(false)}
+          />
+        </div>
+      </div>
+    </div>
+  );
 };
 
 export default Cart;
