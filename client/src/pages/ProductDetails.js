506d2cb70efa1d28e58417a92764c82d894f3faa
G3RGES <Georgios.Nashaat@gmail.com>
2023-10-28 09:39:03 +0200

    Update ProductDetails.js

--- client/src/pages/ProductDetails.js
+++ client/src/pages/ProductDetails.js
@@ -58,7 +58,7 @@ const ProductDetails = () => {
               <div className="text-3xl text-accent font-semibold">
                 ${data[0].attributes.price}{" "}
               </div>
-              <button className="btn btn-accent" onClick={addToCart}>
+              <button className="btn btn-accent" onClick={() => addToCart}>
                 Add to cart
               </button>
             </div>
